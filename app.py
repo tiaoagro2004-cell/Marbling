d35878b16ca8d4bad286f748879d8102ff5f4cbf
requirements.txt <tiaoagro2004@gmail.com>
2026-02-08 11:26:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ with col_res:
     if arquivo and st.button("Executar Análise"):
         # Ajuste de ROI: Pegando um quadrado central menor para evitar a gordura de cobertura (capa)
         h, w, _ = img.shape
-        # Recorte de 35% a 75% para focar no miolo do músculo
-        r_start, r_end = int(h * 0.35), int(h * 0.75)
-        c_start, c_end = int(w * 0.35), int(w * 0.75)
+        # Recorte de 35% a 45% para focar no miolo do músculo
+        r_start, r_end = int(h * 0.35), int(h * 0.45)
+        c_start, c_end = int(w * 0.35), int(w * 0.45)
         amostragem = img[r_start:r_end, c_start:c_end]
 
         # Processamento
